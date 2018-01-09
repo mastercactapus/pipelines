fd6849de5143d94d3dac7d247a0cafb64d4f7343
Nathaniel Caza <mastercactapus@gmail.com>
2018-01-08 23:28:48 -0600

    add easel build

---
+++ easel/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -ex
+
+apt-get update
+apt-get install -y build-essential python p7zip-full cpio
+
+EASEL_VERSION=$(cat src/VERSION)
+NODE_VERSION=$(cat node/VERSION)
+
+
+mkdir -p $HOME/.node-gyp/$NODE_VERSION
+echo 9 >$HOME/.node-gyp/$NODE_VERSION/installVersion
+
+tar xf node/node*-headers.tar.gz --strip-components=1 -C $HOME/.node-gyp/$NODE_VERSION
+tar xf node/node*-linux*.tar.gz --strip-components=1 -C /usr/local
+
+cp src/EaselDriver-$EASEL_VERSION.pkg build-repo/
+
+cd build-repo
+make build/iris-lib/iris.js build/node VERSION=$EASEL_VERSION
+(cd build/iris-lib/node_modules/serialport && /usr/local/lib/node_modules/npm/bin/node-gyp-bin/node-gyp rebuild)
+find build -name node_modules -exec rm -rf {}/.bin \;
+
+cp -r easel.svg easel-driver.desktop AppRun build/. ../bin/
