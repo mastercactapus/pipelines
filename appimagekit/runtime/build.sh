9f17c95ac509aa47f5a2ed5a42e4f191f4eb1511
Nathaniel Caza <mastercactapus@gmail.com>
2018-01-13 15:33:04 -0600

    simplify appimagekit build

--- appimagekit/runtime/build.sh
+++ appimagekit/runtime/build.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 set -e
-apt-get update
-apt-get install -y sudo build-essential wget xxd
 
 cd AppImageKit
-
+ca
 VERSION=$(cat .git/ref)-$(uname -m)
 
-bash -ex install-build-deps.sh
 bash -ex build.sh
 
 mv build/out AppImageKit-$VERSION
