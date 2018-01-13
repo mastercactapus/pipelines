8a245c42c481927292df1872e0ab839c6f2a09c6
Nathaniel Caza <mastercactapus@gmail.com>
2018-01-13 10:10:30 -0600

    amd64 build

--- appimagekit/runtime/build.sh
+++ appimagekit/runtime/build.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 set -e
+apt-get update
+apt-get install -y sudo build-essential wget xxd
+
+cd AppImageKit
+
+VERSION=$(cat .git/ref)-$(uname -m)
+
 bash -ex install-build-deps.sh
 bash -ex build.sh
+
+mv build/out AppImageKit-$VERSION
+tar czf ../bin/AppImageKit-$VERSION.tgz AppImageKit-$VERSION
