e73d960eb7013fff15009e9e5e4da500a5c2c373
Nathaniel Caza <mastercactapus@gmail.com>
2018-01-13 19:02:23 -0600

    split repo

--- easel/driver/build.sh
+++ easel/driver/build.sh
@@ -14,9 +14,9 @@ echo 9 >$HOME/.node-gyp/$NODE_VERSION/installVersion
 tar xf node/node*-headers.tar.gz --strip-components=1 -C $HOME/.node-gyp/$NODE_VERSION
 tar xf node/node*-linux*.tar.gz --strip-components=1 -C /usr/local
 
-cp easel/EaselDriver-$EASEL_VERSION.pkg easel-repo/
+cp easel/EaselDriver-$EASEL_VERSION.pkg easel-driver-repo/
 
-cd easel-repo
+cd easel-driver-repo
 make build/iris-lib/iris.js build/node VERSION=$EASEL_VERSION
 (cd build/iris-lib/node_modules/serialport && /usr/local/lib/node_modules/npm/bin/node-gyp-bin/node-gyp rebuild)
 find build -name node_modules -exec rm -rf {}/.bin \;
@@ -26,5 +26,5 @@ cp easel.svg easel-driver.desktop AppRun build/
 mksquashfs build build.squashfs -root-owned -noappend
 
 FILE=../bin/EaselDriver-$EASEL_VERSION-$(dpkg --print-architecture).AppImage
-cat ../appimagekit-bin/runtime build.squashfs >$FILE
+cat ../appimagekit-bin/*/runtime build.squashfs >$FILE
 chmod a+x $FILE
