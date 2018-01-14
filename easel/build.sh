5cc2afea49aff49bb8b5a2dca05ebf2a76b57ed9
Nathaniel Caza <mastercactapus@gmail.com>
2018-01-13 18:06:12 -0600

    fix easel build

--- easel/build.sh
+++ easel/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -ex
 
-apt-get update
-apt-get install -y build-essential python p7zip-full cpio squashfs-tools
+apt-get -qq update
+apt-get -qq install -y build-essential python p7zip-full cpio squashfs-tools
 
-EASEL_VERSION=$(cat src/VERSION)
+EASEL_VERSION=$(cat easel/VERSION)
 NODE_VERSION=$(cat node/VERSION)
 
 
@@ -14,9 +14,9 @@ echo 9 >$HOME/.node-gyp/$NODE_VERSION/installVersion
 tar xf node/node*-headers.tar.gz --strip-components=1 -C $HOME/.node-gyp/$NODE_VERSION
 tar xf node/node*-linux*.tar.gz --strip-components=1 -C /usr/local
 
-cp src/EaselDriver-$EASEL_VERSION.pkg build-repo/
+cp easel/EaselDriver-$EASEL_VERSION.pkg easel-repo/
 
-cd build-repo
+cd easel-repo
 make build/iris-lib/iris.js build/node VERSION=$EASEL_VERSION
 (cd build/iris-lib/node_modules/serialport && /usr/local/lib/node_modules/npm/bin/node-gyp-bin/node-gyp rebuild)
 find build -name node_modules -exec rm -rf {}/.bin \;
@@ -25,6 +25,6 @@ cp easel.svg easel-driver.desktop AppRun build/
 
 mksquashfs build build.squashfs -root-owned -noappend
 
-FILE=../bin/EaselDriver-$EASEL_VERSION-x86_64.AppImage
-cat ../AppImageKit/runtime-x86_64 build.squashfs >$FILE
+FILE=../bin/EaselDriver-$EASEL_VERSION-$(uname -m).AppImage
+cat ../appimagekit-bin/runtime build.squashfs >$FILE
 chmod a+x $FILE
