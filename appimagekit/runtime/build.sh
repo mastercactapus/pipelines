b47220f21a5cf65eee6adf973240e65710513899
Nathaniel Caza <mastercactapus@gmail.com>
2018-01-13 18:00:09 -0600

    disable appimagekit dep install

--- appimagekit/runtime/build.sh
+++ appimagekit/runtime/build.sh
@@ -4,7 +4,7 @@ set -e
 cd AppImageKit
 VERSION=$(cat .git/ref)-$(uname -m)
 
-bash -ex build.sh
+bash -ex build.sh -n
 
 mv build/out AppImageKit-$VERSION
 tar czf ../bin/AppImageKit-$VERSION.tgz AppImageKit-$VERSION
