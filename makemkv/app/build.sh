4c3ee284883f03949a401be9f06b48478dac4729
Nathaniel Caza <mastercactapus@gmail.com>
2018-01-13 21:04:10 -0600

    makemkv image

--- makemkv/app/build.sh
+++ makemkv/app/build.sh
@@ -4,19 +4,33 @@ set -xe
 apt-get -qq update
 apt-get -qq install -y libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev libqt4-dev
 
-VERSION=$(cat src/VERSION)
+VERSION=$(cat makemkv/VERSION)
 OUTDIR=$(pwd)/bin
+mkdir -p appdir
 
-tar xfv makemkv/makemkv-bin-$VERSION.tar.gz
+tar xf makemkv/makemkv-bin-$VERSION.tar.gz
 cd makemkv-bin-$VERSION
 mkdir -p tmp
 touch tmp/eula_accepted
 make
+make install DESTDIR=../appdir
+cd ..
 
-mkdir -p usr/share/MakeMKV
-install -m 644 src/eula_en_linux.txt usr/share/MakeMKV/
-
-tar xfv makemkv/makemkv-oss-$VERSION.tar.gz
+tar xf makemkv/makemkv-oss-$VERSION.tar.gz
 cd makemkv-oss-$VERSION
 ./configure
-make
+make -j$(nproc) >build.log
+make install DESTDIR=../appdir
+cd ..
+
+# fix version
+sed -i "s/^Version=.*/Version=$VERSION/" appdir/usr/share/applications/makemkv.desktop
+
+install -m 755 linuxdeployqt/*.AppImage /usr/bin/linuxdeployqt
+
+linuxdeployqt appdir/usr/share/applications/makemkv.desktop -bundle-non-qt-libs
+linuxdeployqt appdir/usr/share/applications/makemkv.desktop -bundle-non-qt-libs -executable appdir/usr/bin/mmdtsdec
+linuxdeployqt appdir/usr/share/applications/makemkv.desktop -bundle-non-qt-libs -executable appdir/usr/bin/makemkvcon
+linuxdeployqt appdir/usr/share/applications/makemkv.desktop -appimage
+
+cp *.AppImage bin/
